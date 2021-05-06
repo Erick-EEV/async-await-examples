c5935974b3e456df39e17caefd04d2fb978d69c7
Erick-EEV <EEVERICKEEV@gmail.com>
2021-05-06 17:17:29 -0400

    makeRequest promise

--- async_await_examples/src/component/Await.jsx
+++ async_await_examples/src/component/Await.jsx
@@ -4,22 +4,22 @@ export default function Await() {
 
 
  
-    const makeRequest = async () => {
-        await callAPromise()
-        await callAPromise()
-        await callAPromise()
-        await callAPromise()
-        await callAPromise()
-        throw new Error("oops");
+    const makeRequest = () => {
+        return callAPromise()
+          .then(() => callAPromise())
+          .then(() => callAPromise())
+          .then(() => callAPromise())
+          .then(() => callAPromise())
+          .then(() => {
+            throw new Error("oops");
+          })
       }
       
       makeRequest()
         .catch(err => {
           console.log(err);
           // output
-          // Error: oops at makeRequest (index.js:7:9)
-        })
-
+          // Error: oops at callAPromise.then.then.then.then.then (index.js:8:13)
 
 
     return (
